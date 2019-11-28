475ded5915c1a91defa47eca7e6dada4d80e1e9d
Alexander Jaworowski <alexander@jaworowski.se>
2019-11-28 14:32:11 +0100

    More general Makefile

--- cp_modules_to_destination.sh
+++ cp_modules_to_destination.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Copy our snd files to the /lib/modules/... directory"
-cp snd-soc-rpi-dac.ko /lib/modules/4.4.34+/kernel/sound/soc/bcm/
-cp snd-soc-pcm1794a.ko /lib/modules/4.4.34+/kernel/sound/soc/codecs/
+cp snd-soc-rpi-dac.ko /lib/modules/$(shell uname -r)/kernel/sound/soc/bcm/
+cp snd-soc-pcm1794a.ko /lib/modules/$(shell uname -r)/kernel/sound/soc/codecs/
