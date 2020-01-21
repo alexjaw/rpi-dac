d0ea243b0bff94c31f1b06954c505bfd0670b099
Alexander Jaworowski <alexander@jaworowski.se>
2020-01-21 17:16:10 +0100

    Fix for an error in cp_modules_to_destination.sh

--- cp_modules_to_destination.sh
+++ cp_modules_to_destination.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Copy our snd files to the /lib/modules/... directory"
-cp snd-soc-pcm1794a.ko /lib/modules/$(shell uname -r)/kernel/sound/soc/codecs/
+sudo cp snd-soc-pcm1794a.ko /lib/modules/$(uname -r)/kernel/sound/soc/codecs/
